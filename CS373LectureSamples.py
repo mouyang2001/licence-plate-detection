0d5a77faee9707f0da184e8107c5faac7768e9fe
Matthew Ouyang <mouyang2001@gmail.com>
2022-05-21 16:48:55 +1200

    add 3x3 mean filter and histogram compute functions

--- CS373LectureSamples.py
+++ CS373LectureSamples.py
@@ -4,10 +4,13 @@ from matplotlib import pyplot
 import imageIO.png
 
 # this function reads an RGB color png file and returns width, height, as well as pixel arrays for r,g,b
+
+
 def readRGBImageToSeparatePixelArrays(input_filename):
     image_reader = imageIO.png.Reader(filename=input_filename)
     # png reader gives us width and height, as well as RGB data in image_rows (a list of rows of RGB triplets)
-    (image_width, image_height, rgb_image_rows, rgb_image_info) = image_reader.read()
+    (image_width, image_height, rgb_image_rows,
+     rgb_image_info) = image_reader.read()
 
     print("read image width={}, height={}".format(image_width, image_height))
 
@@ -43,11 +46,11 @@ def readRGBImageToSeparatePixelArrays(input_filename):
 
 
 # a useful shortcut method to create a list of lists based array representation for an image, initialized with a value
-def createInitializedGreyscalePixelArray(image_width, image_height, initValue = 0):
+def createInitializedGreyscalePixelArray(image_width, image_height, initValue=0):
 
-    new_array = [[initValue for x in range(image_width)] for y in range(image_height)]
+    new_array = [[initValue for x in range(
+        image_width)] for y in range(image_height)]
     return new_array
-
 
 
 def computeHistogram(pixel_array, image_width, image_height, nr_bins):
@@ -55,35 +58,54 @@ def computeHistogram(pixel_array, image_width, image_height, nr_bins):
     histogram = [0.0 for i in range(nr_bins)]
 
     # your task to compute the correct histogram!
-
-    # incorrect histogram
-    histogram[0] = 10
-    histogram[32] = 15
-    histogram[63] = 10
+    step_size = 256 / nr_bins
+    for r in range(image_height):
+        for c in range(image_width):
+            histogram[int(pixel_array[r][c] / step_size)] += 1.0
 
     return histogram
+
+
+def computeMean3x3(pixel_array, image_width, image_height):
+    mean_array = createInitializedGreyscalePixelArray(image_width, image_height)
+
+    for y in range(1, image_height-1):
+        for x in range(1, image_width-1):
+            output_pixel = 0.0
+            for eta in [-1, 0, 1]:
+                for xi in [-1, 0, 1]:
+                    output_pixel += pixel_array[y + eta][x + xi]
+            output_pixel /= 9.0
+
+            mean_array[y][x] = int(output_pixel)
+
+    return mean_array
 
 
 def main():
     filename = "krakow.png"
 
-    (image_width, image_height, px_array_r, px_array_g, px_array_b) = readRGBImageToSeparatePixelArrays(filename)
+    (image_width, image_height, px_array_r, px_array_g,
+     px_array_b) = readRGBImageToSeparatePixelArrays(filename)
 
     pixel_array = px_array_r
 
-    fig1, axs1 = pyplot.subplots(2, 1)
+    filtered_image = computeMean3x3(pixel_array, image_width, image_height)
 
+    fig1, axs1 = pyplot.subplots(2, 1)
     axs1[0].set_title('Input image')
     axs1[0].imshow(pixel_array, cmap='gray')
 
     nr_bins = 64
-    dummy_histogram = computeHistogram(pixel_array, image_width, image_height, nr_bins)
+    # dummy_histogram = computeHistogram(pixel_array, image_width, image_height, nr_bins)
 
-    axs1[1].set_title('Histogram')
-    axs1[1].bar(range(nr_bins), dummy_histogram)
+    axs1[1].set_title('Mean')
+    axs1[1].imshow(filtered_image, cmap='gray')
+
+    # axs1[1].set_title('Histogram')
+    # axs1[1].bar(range(nr_bins), dummy_histogram)
 
     pyplot.show()
-
 
 
 if __name__ == "__main__":
