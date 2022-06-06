b8bc95439de3a95b489b818efe7e13148b4ea418
Matthew Ouyang <mouyang2001@gmail.com>
2022-06-06 23:40:27 +1200

    create python scripts to process all number plates

---
+++ run.py
@@ -0,0 +1,8 @@
+import subprocess
+subprocess.call("python CS373LicensePlateDetection.py numberplate1.png")
+subprocess.call("python CS373LicensePlateDetection.py numberplate2.png")
+subprocess.call("python CS373LicensePlateDetection.py numberplate3.png")
+subprocess.call("python CS373LicensePlateDetection.py numberplate4.png")
+subprocess.call("python CS373LicensePlateDetection.py numberplate5.png")
+subprocess.call("python CS373LicensePlateDetection.py numberplate6.png")
+
---
+++ run_extension.py
@@ -0,0 +1,7 @@
+import subprocess
+subprocess.call("python CS373_extension.py numberplate1.png")
+subprocess.call("python CS373_extension.py numberplate2.png")
+subprocess.call("python CS373_extension.py numberplate3.png")
+subprocess.call("python CS373_extension.py numberplate4.png")
+subprocess.call("python CS373_extension.py numberplate5.png")
+subprocess.call("python CS373_extension.py numberplate6.png")
