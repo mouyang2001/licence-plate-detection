6b30a425a4493b043c83d0634899ced361635daa
Matthew Ouyang <mouyang2001@gmail.com>
2022-06-07 00:04:54 +1200

    udpate run.py to support both extension and normal detection script batch job

--- run.py
+++ run.py
@@ -1,8 +1,24 @@
 import subprocess
-subprocess.call("python CS373LicensePlateDetection.py numberplate1.png")
-subprocess.call("python CS373LicensePlateDetection.py numberplate2.png")
-subprocess.call("python CS373LicensePlateDetection.py numberplate3.png")
-subprocess.call("python CS373LicensePlateDetection.py numberplate4.png")
-subprocess.call("python CS373LicensePlateDetection.py numberplate5.png")
-subprocess.call("python CS373LicensePlateDetection.py numberplate6.png")
+import sys
 
+args = sys.argv[1:]
+
+NUMBER_OF_IMAGES = 6
+
+def main():
+    script_name = "CS373LicensePlateDetection.py"
+    if args != [] and args[0] == '-extension':
+        script_name = "CS373_extension.py"
+    
+    commands = []
+
+    for i in range(NUMBER_OF_IMAGES):
+        commands.append("python " + script_name + " numberplate" + str(i + 1) + ".png")
+
+    processes = [subprocess.Popen(cmd, shell=True) for cmd in commands]
+    for p in processes: 
+        p.wait()
+
+
+if __name__ == "__main__":
+    main()
